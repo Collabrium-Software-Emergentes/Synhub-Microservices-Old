97c6ebc2c5174fd2c9353da2d1da393da4aa4cd6
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-26 18:09:33 -0500

    feat(media): implement ImageCommandServiceImpl for image upload and deletion
    - Added `ImageCommandServiceImpl` to handle `UploadGroupImageCommand` and `DeleteGroupImageCommand`.
    - Integrated Cloudinary for image upload and deletion operations.
    - Validated group existence through `GroupsQueryPort` before processing commands.

---
+++ media/src/main/java/com/collabrium/media/media/management/application/internal/commandservices/ImageCommandServiceImpl.java
@@ -0,0 +1,96 @@
+package com.collabrium.media.media.management.application.internal.commandservices;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.collabrium.media.media.management.application.internal.outboundservices.ports.GroupsQueryPort;
+import com.collabrium.media.media.management.domain.model.commands.DeleteGroupImageCommand;
+import com.collabrium.media.media.management.domain.model.commands.UploadGroupImageCommand;
+import com.collabrium.media.media.management.domain.model.responses.ImageUploadResponse;
+import com.collabrium.media.media.management.domain.services.ImageCommandService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class ImageCommandServiceImpl implements ImageCommandService {
+
+  private final Cloudinary cloudinary;
+  private final GroupsQueryPort groupsQueryPort;
+
+  public ImageCommandServiceImpl(
+      Cloudinary cloudinary,
+      GroupsQueryPort groupsQueryPort
+  ) {
+
+    this.cloudinary = cloudinary;
+    this.groupsQueryPort = groupsQueryPort;
+  }
+
+  @Override
+  public Optional<ImageUploadResponse> handle(UploadGroupImageCommand command) {
+
+    var group = groupsQueryPort.getGroupById(command.groupId());
+
+    if (group == null) {
+      throw new RuntimeException("Group not found");
+    }
+
+    try {
+
+      Map<String, Object> result =
+          cloudinary.uploader().upload(
+              command.file().getBytes(),
+              ObjectUtils.asMap(
+                  "folder", "synhub/groups",
+                  "resource_type", "image",
+                  "overwrite", false
+              )
+          );
+
+      var imageResponse = mapResponse(result);
+
+      return Optional.of(imageResponse);
+
+    } catch (Exception e) {
+
+      throw new RuntimeException(
+          "Error uploading image: " + e.getMessage(),
+          e
+      );
+    }
+  }
+
+  @Override
+  public void handle(DeleteGroupImageCommand command) {
+
+    var group = groupsQueryPort.getGroupById(command.groupId());
+
+    if (group == null) {
+      throw new RuntimeException("Group not found");
+    }
+
+    try {
+
+      cloudinary.uploader().destroy(
+          command.publicId(),
+          ObjectUtils.emptyMap()
+      );
+
+    } catch (Exception e) {
+
+      throw new RuntimeException(
+          "Error deleting image: " + e.getMessage(),
+          e
+      );
+    }
+
+  }
+
+  private ImageUploadResponse mapResponse(Map<String, Object> result) {
+    return new ImageUploadResponse(
+        result.get("secure_url").toString(),
+        result.get("public_id").toString()
+    );
+  }
+}
