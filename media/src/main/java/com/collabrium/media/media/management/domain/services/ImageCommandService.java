2e358f9ef892727b87417642cd3d84801023a424
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-26 18:09:27 -0500

    feat(media): update ImageCommandService handle method to return Optional
    - Modified `handle(UploadGroupImageCommand)` to return `Optional<ImageUploadResponse>` for improved null safety.

--- media/src/main/java/com/collabrium/media/media/management/domain/services/ImageCommandService.java
+++ media/src/main/java/com/collabrium/media/media/management/domain/services/ImageCommandService.java
@@ -4,9 +4,11 @@ import com.collabrium.media.media.management.domain.model.commands.DeleteGroupIm
 import com.collabrium.media.media.management.domain.model.commands.UploadGroupImageCommand;
 import com.collabrium.media.media.management.domain.model.responses.ImageUploadResponse;
 
+import java.util.Optional;
+
 public interface ImageCommandService {
 
-  ImageUploadResponse handle(UploadGroupImageCommand command);
+  Optional<ImageUploadResponse> handle(UploadGroupImageCommand command);
 
   void handle(DeleteGroupImageCommand command);
 }
