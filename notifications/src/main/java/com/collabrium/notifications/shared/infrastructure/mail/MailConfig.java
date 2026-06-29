b92d52031814adf9ee869449c0690996d27cac39
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-29 12:41:26 -0500

    feat(notifications): add MailConfig and RabbitMQConfig classes for infrastructure setup

---
+++ notifications/src/main/java/com/collabrium/notifications/shared/infrastructure/mail/MailConfig.java
@@ -0,0 +1,4 @@
+package com.collabrium.notifications.shared.infrastructure.mail;
+
+public class MailConfig {
+}
---
+++ notifications/src/main/java/com/collabrium/notifications/shared/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,4 @@
+package com.collabrium.notifications.shared.infrastructure.rabbitmq;
+
+public class RabbitMQConfig {
+}
