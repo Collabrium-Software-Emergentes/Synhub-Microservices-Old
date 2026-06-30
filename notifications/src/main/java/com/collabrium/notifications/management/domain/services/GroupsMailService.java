849e6647dd07feb61f70797a6180e924c321f85e
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-30 15:36:42 -0500

    feat(notifications): extend GroupsMailService to handle invitation accepted emails

--- notifications/src/main/java/com/collabrium/notifications/management/domain/services/GroupsMailService.java
+++ notifications/src/main/java/com/collabrium/notifications/management/domain/services/GroupsMailService.java
@@ -1,8 +1,11 @@
 package com.collabrium.notifications.management.domain.services;
 
 import com.collabrium.notifications.management.domain.model.commands.SendGroupCreatedEmailCommand;
+import com.collabrium.notifications.management.domain.model.commands.SendInvitationAcceptedEmailCommand;
 
 public interface GroupsMailService {
 
   void handle(SendGroupCreatedEmailCommand command);
+
+  void handle(SendInvitationAcceptedEmailCommand command);
 }
