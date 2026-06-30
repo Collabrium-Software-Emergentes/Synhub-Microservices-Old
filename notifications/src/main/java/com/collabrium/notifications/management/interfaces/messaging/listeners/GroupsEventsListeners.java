8f644027daaa78db4e95e538166121e172d20e63
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-30 16:00:11 -0500

    feat(notifications): add listener for InvitationAcceptedEvent and assembler logic for email commands

--- notifications/src/main/java/com/collabrium/notifications/management/interfaces/messaging/listeners/GroupsEventsListeners.java
+++ notifications/src/main/java/com/collabrium/notifications/management/interfaces/messaging/listeners/GroupsEventsListeners.java
@@ -1,12 +1,15 @@
 package com.collabrium.notifications.management.interfaces.messaging.listeners;
 
 import com.collabrium.notifications.management.domain.model.events.GroupCreatedEvent;
+import com.collabrium.notifications.management.domain.model.events.InvitationAcceptedEvent;
 import com.collabrium.notifications.management.domain.services.GroupsMailService;
 import com.collabrium.notifications.management.interfaces.messaging.transform.SendGroupCreatedEmailCommandFromEventAssembler;
+import com.collabrium.notifications.management.interfaces.messaging.transform.SendInvitationAcceptedEmailCommandFromEventAssembler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import static com.collabrium.notifications.shared.infrastructure.rabbitmq.RabbitMQConfig.GROUP_CREATED_QUEUE;
+import static com.collabrium.notifications.shared.infrastructure.rabbitmq.RabbitMQConfig.INVITATION_ACCEPTED_QUEUE;
 
 @Component
 public class GroupsEventsListeners {
@@ -27,5 +30,14 @@ public class GroupsEventsListeners {
         SendGroupCreatedEmailCommandFromEventAssembler.toCommandFromEvent(event);
 
     groupsMailService.handle(sendGroupCreatedEmailCommand);
+  }
+
+  @RabbitListener(queues = INVITATION_ACCEPTED_QUEUE)
+  public void handle(InvitationAcceptedEvent event) {
+
+    var sendInvitationAcceptedEmailCommand =
+        SendInvitationAcceptedEmailCommandFromEventAssembler.toCommandFromEvent(event);
+
+    groupsMailService.handle(sendInvitationAcceptedEmailCommand);
   }
 }
