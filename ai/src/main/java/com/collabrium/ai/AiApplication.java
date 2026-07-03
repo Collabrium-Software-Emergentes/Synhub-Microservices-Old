736f7e40fcb57c082ccd2abf5b14dde821039b47
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-07-03 09:54:45 -0500

    feat(ai): enable Feign clients and exclude default UserDetailsService configuration
    Added `@EnableFeignClients` for Feign integrations and excluded `UserDetailsServiceAutoConfiguration` to customize security configuration.

--- ai/src/main/java/com/collabrium/ai/AiApplication.java
+++ ai/src/main/java/com/collabrium/ai/AiApplication.java
@@ -2,8 +2,15 @@ package com.collabrium.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				UserDetailsServiceAutoConfiguration.class
+		}
+)
+@EnableFeignClients
 public class AiApplication {
 
 	public static void main(String[] args) {
