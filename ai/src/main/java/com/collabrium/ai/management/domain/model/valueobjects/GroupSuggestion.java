3049a31878b2e64cfc0accbe7fdf085fbc1da162
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-07-04 11:53:24 -0500

    feat(ai): rename fields in GroupSuggestion value object

--- ai/src/main/java/com/collabrium/ai/management/domain/model/valueobjects/GroupSuggestion.java
+++ ai/src/main/java/com/collabrium/ai/management/domain/model/valueobjects/GroupSuggestion.java
@@ -1,7 +1,7 @@
 package com.collabrium.ai.management.domain.model.valueobjects;
 
 public record GroupSuggestion(
-    String name,
-    String description
+    String groupName,
+    String groupDescription
 ) {
 }
